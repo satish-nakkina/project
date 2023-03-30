a56b90dc17496cbfcb3dc75c4b4b5d832e6714fa
satish nakkina <satish.nakkina@zemosolabs.com>
2023-03-30 11:40:30 +0530

    m

---
+++ app.py
@@ -0,0 +1,91 @@
+from __future__ import division, print_function
+# coding=utf-8
+from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+
+import sys
+import os
+import glob
+import re
+import numpy as np
+import random
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from PIL import  Image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+import model
+# Define a flask app
+app = Flask(__name__)
+account_sid = ''
+auth_token = ''
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+@app.route('/', methods=['GET'])
+def index():
+   
+    return render_template('index.html')
+def process_image(file):
+    image = Image.open(file)
+    image = image.resize((64, 64))
+    image = np.array(image)
+    if len(image.shape) == 2:  # if image has only one channel, repeat along the channel axis
+        image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+    else:
+        image = image[:, :, :3]  # take only the first 3 channels if there are more than 3
+    image = image / 255.0
+    return image[np.newaxis, ...]
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        image=process_image(file_path)
+        tempr=random.randint(50,101)
+        fire = model.predict(image)
+        print(fire)
+        if fire:
+            if tempr>70: 
+                prediction = 'fire'           
+            else:
+                prediction = 'fire'
+        else:
+            prediction = 'no fire'
+
+        return prediction
+        # classifier = load_model('classifier.h5')
+        # test_image = image.load_img(file_path, target_size = (64, 64))
+        # test_image = image.img_to_array(test_image)
+        # test_image = np.expand_dims(test_image, axis = 0)
+        # result = classifier.predict(test_image)
+        # #training_set.class_indices
+        # if result[0][0] == 1:
+        #     prediction = 'notfire'
+        # else:
+        #     prediction = 'fire'
+        # return prediction 
+  
+    return None
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+    # Serve the app with gevent
+    #http_server = WSGIServer(('', 5000), app)
+    #http_server.serve_forever()
---
+++ model.py
@@ -0,0 +1,126 @@
+
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Load the pre-trained fire detection model
+classifier = load_model('classifier.h5')
+
+def preprocess_image(img):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+    # Normalize the pixel values to [0, 1]
+    gray = gray / 255.0
+    
+    return gray
+
+def predict_part(inp_arr_image):
+    # Preprocess the input image
+    test_image = cv2.resize(inp_arr_image, (64, 64))
+    test_image = preprocess_image(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    test_image = np.expand_dims(test_image, axis=-1)  # add channel dimension
+    
+    # Make a prediction using the pre-trained model
+    result = classifier.predict(test_image)
+    if result[0][0] < 0.5:
+        # The model predicts that the input image contains fire
+        return True
+    else:
+        # The model predicts that the input image does not contain fire
+        return False
+
+def get_cells_img(np_arr_img, n=64):
+    # Split the input image into small cells of size n x n
+    sub_imgs = []
+    for row in range((np_arr_img.shape[0] // n) + 1):
+        for col in range((np_arr_img.shape[1] // n) + 1):
+            c_0 = col * n
+            c_1 = min((c_0 + n), np_arr_img.shape[1])
+            r_0 = row * n
+            r_1 = min((r_0 + n), np_arr_img.shape[0])
+            sub_imgs.append(np_arr_img[r_0:r_1, c_0:c_1, :])
+    return sub_imgs
+
+def predict(img_path):
+    # Load the input image
+    # inp_img = cv2.imread(img_path)
+    # print(img_path)
+    inp_img = cv2.cvtColor(img_path, cv2.COLOR_BGR2RGB)
+
+    # Split the input image into cells and make predictions on each cell
+    fire_pred = [predict_part(img) for img in get_cells_img(inp_img, n=128)]
+
+    # Count the number of cells predicted to contain fire
+    fire_cnt = sum(fire_pred)
+
+    # Make a final prediction based on the number of cells predicted to contain fire
+    if fire_cnt > 10:
+        # If more than 10 cells contain fire, return True (fire detected)
+        return True
+    else:
+        # Otherwise, return False (no fire detected)
+        return False
+
+
+# import numpy as np
+# import cv2
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+
+# # Load the pre-trained fire detection model
+# classifier = load_model('classifier.h5')
+
+# def get_img_array(path):
+#     # Load the input image and convert it to a NumPy array
+#     img = image.load_img(path, target_size=(500, 750))
+#     img_array = image.img_to_array(img)
+#     return img_array
+
+# def predict_part(inp_arr_image):
+#     # Preprocess the input image
+#     test_image = cv2.resize(inp_arr_image, (64, 64))
+#     test_image = np.expand_dims(test_image, axis=0)
+#     test_image = test_image / 255.0  # normalize pixel values
+
+#     # Make a prediction using the pre-trained model
+#     result = classifier.predict(test_image)
+#     if result[0][0] < 0.9:
+#         # The model predicts that the input image contains fire
+#         return True
+#     else:
+#         # The model predicts that the input image does not contain fire
+#         return False
+
+# def get_cells_img(np_arr_img, n=64):
+#     # Split the input image into small cells of size n x n
+#     sub_imgs = []
+#     for row in range((np_arr_img.shape[0] // n) + 1):
+#         for col in range((np_arr_img.shape[1] // n) + 1):
+#             c_0 = col * n
+#             c_1 = min((c_0 + n), np_arr_img.shape[1])
+#             r_0 = row * n
+#             r_1 = min((r_0 + n), np_arr_img.shape[0])
+#             sub_imgs.append(np_arr_img[r_0:r_1, c_0:c_1, :])
+#     return sub_imgs
+
+# def predict(img_path):
+#     # Load the input image
+#     inp_img = get_img_array(img_path)
+
+#     # Split the input image into cells and make predictions on each cell
+#     fire_pred = [predict_part(img) for img in get_cells_img(inp_img, n=128)]
+
+#     # Count the number of cells predicted to contain fire
+#     fire_cnt = sum(fire_pred)
+
+#     print(fire_cnt)
+#     # Make a final prediction based on the number of cells predicted to contain fire
+#     if fire_cnt > 10:
+#         # If more than 10 cells contain fire, return True (fire detected)
+#         return True
+#     else:
+#         # Otherwise, return False (no fire detected)
+#         return False
